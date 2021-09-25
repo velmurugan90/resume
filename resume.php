95f7fb4cc6b8d7f7a5c2046c9b0d21dd57320677
velmurugan90 <79251278+velmurugan90@users.noreply.github.com>
2021-09-24 18:33:40 -1200

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,149 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Velmurugan Resume</title>
+<style>
+      body {
+        background-color: rgb(221, 214, 205);
+      }
+      #top {
+        background-color: lightgray;
+      }
+      h1 {
+        margin-left: 45%;
+color: mediumseagreen;
+        font-size: 40px;
+      }
+      #top address {
+        margin-left: 85%;
+        font-size: 20px;
+color: indigo;
+      }
+
+      #top h2 {
+        font-size: 30px;
+        text-transform: uppercase;
+color: mediumslateblue;
+      }
+      h2 {
+color: maroon;
+        font-style: italic;
+        background-color: salmon;
+      }
+th {
+color: mediumblue;
+        font-size: 20px;
+      }
+      footer {
+        background-color: papayawhip;
+      }
+
+      #foot {
+        margin-left: 85%;
+      }
+</style>
+</head>
+<body>
+<h1>RESUME</h1>
+<div id="top">
+<h2>Velmurugan.M</h2>
+<address>
+46,ChettyStret<br />
+Varakalpattu, <br />
+Cuddalore - 607 109.
+</address>
+<p>
+        Mobile:9092087997,7449279703 <br />
+E-mail:velmuru74492@gmail.com
+</p>
+</div>
+<hr />
+<h2>CARRIER OBJECTIVE:</h2>
+<p>
+      To attain a unique identity in your educational institution through hard
+work,creativity and to contribute my talents and hard work for the growth
+      of your institution.
+</p>
+<h2>EDUCATIONAL QUALIFICATION:</h2>
+<table border="4" cellpadding="15">
+<tr>
+<th>Course Name</th>
+<th>Institution</th>
+<th>Year of Passing</th>
+<th>Percentage</th>
+</tr>
+<tr>
+<td>BCA</td>
+<td>St.Joseph's College of arts and science (Autonomous),Cuddalore</td>
+<td>2019</td>
+<td>84%</td>
+</tr>
+<tr>
+<td>HSC</td>
+<td>ARLM MarticulationHr.Sec.School,Cuddalore</td>
+<td>2016</td>
+<td>71%</td>
+</tr>
+<tr>
+<td>SSLC</td>
+<td>Krishnasamy Matric Hr.Sec.School of Excellent,Cuddalore</td>
+<td>2014</td>
+<td>85%</td>
+</tr>
+</table>
+<h2>COMPUTER knowledge</h2>
+<ul>
+<li>Computer Basic:MS-Word,MS-Powerpoint,MS-Excel</li>
+<li>C,C++</li>
+<li>Java,Oracle</li>
+</ul>
+<h2>CO-CURRICULAR ACTIVITIES:</h2>
+<ul>
+<li>Village Service Club(VSL)</li>
+</ul>
+<h2>CHARACTERS ABOUT ME:</h2>
+<ul>
+<li>Very much eagar to work sincerely.</li>
+<li>Positive attitude.</li>
+<li>Self confidentperson.</li>
+</ul>
+<h2>PERSONAL DETAILS:</h2>
+<p>
+      Name: VELMURUGAN K <br />
+      Father's Name: aaa<br />
+      Mother's Name: bbb<br />
+      Date of birth: 23.04.1999 <br />
+      Gender: Male <br />
+      Marital Status: Single <br />
+      Religion: Hindu <br />
+      Languages: Tamil and English <br />
+</p>
+<h2>DECLARATION</h2>
+<p>
+      Hereby, I assure that the information furnished above is true.If I get an
+      opportunity to work in your concern, I assur that I shall discharge my
+      duties to the fullest satisfication of my Employer and to the best of my
+      ability.
+</p>
+<footer>
+<p>
+Place:Cuddalore<br />
+<br />
+        Date:
+</p>
+<div id="foot">
+        Yours Sincirely, <br />
+        (VELMURUGAN M)
+</div>
+</footer>
+</body>
+</html>
+
+
+
+
+
+
+
+
